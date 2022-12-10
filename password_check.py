6616ccd68c6f71aa44e02b8ff68701a8b0cbd570
Attica-oss <g.mounac@outlook.com>
2022-12-10 21:40:00 +0400

    adding starting files

---
+++ password_check.py
@@ -0,0 +1,78 @@
+''' Application to check if Password have been Pwned!'''
+
+import requests
+import hashlib
+import sys
+import csv
+
+def request_api_data(query_char):
+    """_summary_
+
+    Args:
+        query_char (_type_): _description_
+
+    Raises:
+        RuntimeError: _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code} check the API and retry.')
+    return res
+
+
+def get_password_leak_count(hashes,hash_to_check):
+    ''' gets the count of leaks from the API'''
+
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    """_check if the password is in the response data_
+
+    Args:
+        password (_type_): _description_
+
+    Returns:
+        _type_: Gets the count of leaks (if available)
+    """
+
+    passwordsha1 = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5char, tail = passwordsha1[:5],passwordsha1[5:]
+    response = request_api_data(first5char)
+    return get_password_leak_count(response,tail)
+
+def open_csv(filename):
+
+    with open(filename, mode = 'r',newline='') as file:
+        
+        reader = csv.reader(file, delimiter = ',')
+        
+        # store the headers in a separate variable,
+        headings = next(reader)
+        
+        # output list to store all rows
+        passwords =[]
+        for row in reader:
+            passwords.append(row[1])
+        return passwords
+# open_csv('password.csv') 
+
+def main(file):
+    passwords = open_csv(file)
+    for i in passwords:
+        count = pwned_api_check(i)
+        if count:
+           print(f'{i} has been found {count} times...')
+        else:
+            print(f'{i} has NOT been found.')
+    return 'done.'
+
+main('password.csv')
